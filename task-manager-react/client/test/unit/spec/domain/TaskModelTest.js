ebdc73753eb4c25a52ae9b60fd471a70b2597ff4
Daniel Johnson <dan@balancerockmedia.com>
2015-04-09 19:48:19 -0400

    edits

--- task-manager-react/client/test/unit/spec/domain/TaskModelTest.js
+++ task-manager-react/client/test/unit/spec/domain/TaskModelTest.js
@@ -1,12 +1,41 @@
+var TaskModel = require('../../../../babelified/domain/TaskModel.js');
+
 describe('TaskModel', function() {
     'use strict';
 
     beforeEach(function() {
+        spyOn($, 'ajax');
         
+        this.taskModel = new TaskModel();
+        this.taskModel.set('id', 1);
     });
 
-    it('can add', function() {
-        expect(1+1).toBe(2);
+    it('toggleStatus() from 0 to 2', function() {
+        this.taskModel.set('status', 0);
+
+        this.taskModel.toggleStatus();
+
+        expect($.ajax).toHaveBeenCalledWith(jasmine.objectContaining({
+            type: 'PUT',
+            url: 'http://localhost:3000/tasks/1'
+        }));
+
+        expect(this.taskModel.get('status')).not.toBe(0);
+        expect(this.taskModel.get('status')).toBe(2);
+    });
+
+    it('toggleStatus() from 2 to 0', function() {
+        this.taskModel.set('status', 2);
+
+        this.taskModel.toggleStatus();
+
+        expect($.ajax).toHaveBeenCalledWith(jasmine.objectContaining({
+            type: 'PUT',
+            url: 'http://localhost:3000/tasks/1'
+        }));
+
+        expect(this.taskModel.get('status')).not.toBe(2);
+        expect(this.taskModel.get('status')).toBe(0);
     });
 
 });
