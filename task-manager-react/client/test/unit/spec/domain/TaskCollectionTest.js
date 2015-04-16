12e3a051323db3d6cec9d14f4d83f7e1d94bb506
Daniel Johnson <dan@balancerockmedia.com>
2015-04-15 22:07:49 -0400

    edits

---
+++ task-manager-react/client/test/unit/spec/domain/TaskCollectionTest.js
@@ -0,0 +1,10 @@
+var TaskCollection = require('../../../../babelified/domain/TaskCollection.js');
+
+describe('TaskCollection', function() {
+    'use strict';
+
+    it('can add', function() {
+        expect(1+1).toBe(2);
+    });
+
+});
